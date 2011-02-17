8a76662df476305426f1a3953be83a54d2b24e56
Jim Weirich <jim.weirich@gmail.com>
2011-02-16 19:53:01 -0500

    Removed conditionals for backstage pass

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -72,37 +72,16 @@ class QualityUpdater
       return
     end
 
-    if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        item.quality -= 1
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
+    if item.quality > 0
+      item.quality -= 1
     end
+
     item.sell_in -= 1
     if item.sell_in < 0
-      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality -= 1
-          end
+      if item.quality > 0
+        if item.name != 'Sulfuras, Hand of Ragnaros'
+          item.quality -= 1
         end
-      else
-        item.quality = item.quality - item.quality
       end
     end
   end

e4c4e3426ce658ce2b32caccbeb35a734f85df50
Jim Weirich <jim.weirich@gmail.com>
2011-02-16 19:43:20 -0500

    Partial Backstage pass updater (tests are incomplete)

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -35,11 +35,34 @@ class QualityUpdater
     end
   end
 
+  class BackstagePassQualityUpdater
+    def update_quality(item)
+      if item.sell_in > 10
+        bump(item, 1)
+      elsif item.sell_in > 5
+        bump(item, 2)
+      # elsif item.sell_in > 0
+      #   bump(item, 3)
+      else
+        item.quality = 0
+      end
+    end
+    def update_sell_in(item)
+      item.sell_in -= 1
+    end
+    def bump(item, amount)
+      item.quality += amount
+      item.quality = 50 if item.quality >= 50
+    end
+  end
+
   def update_one(item)
     if item.name == 'Sulfuras, Hand of Ragnaros'
       updater = NoopQualityUpdater.new
     elsif item.name == 'Aged Brie'
       updater = BrieQualityUpdater.new
+    elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      updater = BackstagePassQualityUpdater.new
     else
       updater = nil
     end

96cb16d431ade67def9e56baaf0a8fe4498d2a3f
Jim Weirich <jim.weirich@gmail.com>
2011-12-09 08:05:44 -0500

    Correct incorrect post-sell date quality adjustment of conjured items.

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -69,7 +69,11 @@ class QualityUpdater
 
   class ConjuredItemQualityUpdater < StandardQualityUpdater
     def update_quality(item)
-      bump(item, -2)
+      if item.sell_in <= 0
+        bump(item, -4)
+      else
+        bump(item, -2)
+      end
     end
   end
 

69d07153be681f85981793711f7ca124d143a18b
James Thompson <james@mavenlink.com>
2017-01-09 15:36:23 -0700

    Updated spec syntax around Aged Brie behavior.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -36,42 +36,48 @@ RSpec.describe '#update_quality' do
       end
     end
 
-    context "Aged Brie" do
-      Given(:name) { "Aged Brie" }
+    context 'Aged Brie' do
+      let(:name) { 'Aged Brie' }
 
-      Invariant { item.sell_in.should == initial_sell_in-1 }
+      before { expect(item.sell_in).to eq(initial_sell_in - 1) }
 
-      context "before sell date" do
-        Then { item.quality.should == initial_quality+1 }
+      context 'before sell date' do
+        it { expect(item.quality).to eq(initial_quality + 1) }
 
-        context "with max quality" do
-          Given(:initial_quality) { 50 }
-          Then { item.quality.should == initial_quality }
+        context 'with max quality' do
+          let(:initial_quality) { 50 }
+
+          it { expect(item.quality).to eq(initial_quality) }
         end
       end
 
-      context "on sell date" do
-        Given(:initial_sell_in) { 0 }
-        Then { item.quality.should == initial_quality+2 }
+      context 'on sell date' do
+        let(:initial_sell_in) { 0 }
 
-        context "near max quality" do
-          Given(:initial_quality) { 49 }
-          Then { item.quality.should == 50 }
+        it { expect(item.quality).to eq(initial_quality + 2) }
+
+        context 'near max quality' do
+          let(:initial_quality) { 49 }
+
+          it { expect(item.quality).to eq(50) }
         end
 
-        context "with max quality" do
-          Given(:initial_quality) { 50 }
-          Then { item.quality.should == initial_quality }
+        context 'with max quality' do
+          let(:initial_quality) { 50 }
+
+          it { expect(item.quality).to eq(initial_quality) }
         end
       end
 
-      context "after sell date" do
-        Given(:initial_sell_in) { -10 }
-        Then { item.quality.should == initial_quality+2 }
+      context 'after sell date' do
+        let(:initial_sell_in) { -10 }
 
-        context "with max quality" do
-          Given(:initial_quality) { 50 }
-          Then { item.quality.should == initial_quality }
+        it { expect(item.quality).to eq(initial_quality + 2) }
+
+        context 'with max quality' do
+          let(:initial_quality) { 50 }
+
+          it { expect(item.quality).to eq(initial_quality) }
         end
       end
     end

934e0b16ce914269851ae6f8f773c47e15df1390
Gabriel Souza <gabriel.souza@locaweb.com.br>
2017-07-27 18:32:04 -0300

    tests

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -31,33 +31,33 @@ def update_quality(items)
       end
     end
 
-#    if item.name != 'Sulfuras, Hand of Ragnaros'
-#      item.sell_in -= 1
-#    end
-#
-#    if item.sell_in < 0
-#      if item.name != "Aged Brie"
-#
-#        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-#
-#          if item.quality > 0
-#            if item.name != 'Sulfuras, Hand of Ragnaros'
-#              item.quality -= 1
-#            end
-#          end
-#
-#        else
-#          item.quality = item.quality - item.quality
-#        end
-#
-#      else
-#
-#        if item.quality < 50
-#          item.quality += 1
-#        end
-#
-#      end
-#    end
+    if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.sell_in -= 1
+    end
+
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+
+        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+
+          if item.quality > 0
+            if item.name != 'Sulfuras, Hand of Ragnaros'
+              item.quality -= 1
+            end
+          end
+
+        else
+          item.quality = item.quality - item.quality
+        end
+
+      else
+
+        if item.quality < 50
+          item.quality += 1
+        end
+
+      end
+    end
   end
 end
 

b2422b28048e3dccace0defcc41a31049bc95da4
James Thompson <james@thomps.onl>
2021-10-20 15:48:50 -0700

    Flatten first conditional block to clarify what item type is in scope.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,11 +1,7 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
+    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros'
+      item.quality -= 1 if item.quality > 0
     else
       if item.quality < 50
         item.quality += 1

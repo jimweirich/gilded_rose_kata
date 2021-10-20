d88e08da883f450afff88f5abd8131a1debadecd
James Thompson <james@thomps.onl>
2021-10-20 16:34:33 -0700

    Extract item type behaviors into discreet update methods.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,29 +10,41 @@ def expired?(item)
   item.sell_in < 0
 end
 
+def update_normal_item(item)
+  decrement_quality(item)
+  item.sell_in -= 1
+  decrement_quality(item) if expired?(item)
+end
+
+def update_backstage_pass(item)
+  if item.sell_in < 6
+    increment_quality(item, 3)
+  elsif item.sell_in < 11
+    increment_quality(item, 2)
+  else
+    increment_quality(item)
+  end
+
+  item.sell_in -= 1
+
+  item.quality = item.quality - item.quality if expired?(item)
+end
+
+def update_aged_brie(item)
+  increment_quality(item)
+  item.sell_in -= 1
+  increment_quality(item) if expired?(item)
+end
+
 def update_quality(items)
   items.each do |item|
     case item.name
     when 'NORMAL ITEM'
-      decrement_quality(item)
-      item.sell_in -= 1
-      decrement_quality(item) if expired?(item)
+      update_normal_item(item)
     when 'Backstage passes to a TAFKAL80ETC concert'
-      if item.sell_in < 6
-        increment_quality(item, 3)
-      elsif item.sell_in < 11
-        increment_quality(item, 2)
-      else
-        increment_quality(item)
-      end
-
-      item.sell_in -= 1
-
-      item.quality = item.quality - item.quality if expired?(item)
+      update_backstage_pass(item)
     when 'Aged Brie'
-      increment_quality(item)
-      item.sell_in -= 1
-      increment_quality(item) if expired?(item)
+      update_aged_brie(item)
     else # Sulfuras
       #No-Op
     end
